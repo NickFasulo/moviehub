29fdc1fa9ec17da1ae28938c566c334988c604a3
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-07-23 10:44:01 -0400

    switching to passport

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
