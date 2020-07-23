c947dab12ef7dcd626700d42c08c5b9b8e3f29dd
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-07-23 12:56:21 -0400

    switched to passport

--- server/lib/passort.js
+++ server/lib/passort.js
@@ -1,18 +1,29 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-
 const User = require('../models/User');
-
 const keys = process.env.JWT_USER_SECRET_KEY;
 
 const jwtOpts = {};
-
 jwtOpts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOpts.secretOrKey = keys;
 
-const userJWTLoginStrategy = new JwtStrategy(
-  jwtOpts,
-  async (payload, done) => {}
-);
+const userJWTLoginStrategy = new JwtStrategy(jwtOpts, async (payload, done) => {
+  const userEmail = payload.email;
+
+  try {
+    if (userEmail) {
+      const user = await User.findOne({ email: userEmail });
+
+      if (!user || user === null) {
+        return done(null, false);
+      }
+      return done(null, user);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+});
+
+// passport.use(userJWTLogin);
 
 module.exports = userJWTLoginStrategy;
