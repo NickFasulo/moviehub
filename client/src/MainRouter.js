3e0edc5ffab9ad9b3fc49f4599f6ddf77a07f90f
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-07-22 23:44:15 -0400

    set up axios, react router, redux

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 // const Signup = React.lazy(() => import('./components/signup/Signup'));
 // const Navbar = React.lazy(() => import('./components/navbar/Navbar'));
 // const Login = React.lazy(() => import('./components/login/Login'));
+const Home = React.lazy(() => import('./components/views/Home/Home'));
 
 export default class MainRouter extends Component {
   render() {
