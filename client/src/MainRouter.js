5d62ea05ac6470411f9e1a060d39298ecd9809e9
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-07-23 00:16:02 -0400

    created auth for react routes

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -5,6 +5,7 @@ import Landing from './components/views/Landing/Landing';
 import Navbar from './components/views/Navbar/Navbar';
 import Footer from './components/views/Footer/Footer';
 import Login from './components/views/Login/Login';
+import Auth from './hoc/auth';
 
 export default class MainRouter extends Component {
   render() {
@@ -13,9 +14,9 @@ export default class MainRouter extends Component {
         <Navbar />
         <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
           <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
+            <Route exact path="/" component={Auth(Landing, null)} />
+            <Route exact path="/login" component={Auth(Login, false)} />
+            <Route exact path="/register" component={Auth(Register, false)} />
           </Switch>
         </div>
         <Footer />
--- client/src/redux/_actions/user_actions.js
+++ client/src/redux/_actions/user_actions.js
@@ -24,17 +24,6 @@ export function loginUser(dataToSubmit) {
   };
 }
 
-export function auth() {
-  const request = axios
-    .get(`${USER_SERVER}/auth`)
-    .then(response => response.data);
-
-  return {
-    type: AUTH_USER,
-    payload: request,
-  };
-}
-
 export function logoutUser() {
   const request = axios
     .get(`${USER_SERVER}/logout`)
@@ -42,6 +31,17 @@ export function logoutUser() {
 
   return {
     type: LOGOUT_USER,
+    payload: request,
+  };
+}
+
+export function auth() {
+  const request = axios
+    .get(`${USER_SERVER}/auth`)
+    .then(response => response.data);
+
+  return {
+    type: AUTH_USER,
     payload: request,
   };
 }
